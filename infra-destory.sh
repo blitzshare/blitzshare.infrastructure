8fd47e7d27b3477c414d1d3a32496a5c884f2d24
kimchi <pavel@nate.tech>
2021-10-24 16:17:14 +0100

    chore: destroy script

--- infra-destory.sh
+++ infra-destory.sh
@@ -1,7 +1,8 @@
+# assuming that state s3 bucket already created
 
 pushd ${PWD}/eu-west-1/eks-cluster
-
-terraform_v1.0.9 init 
+terraform_v1.0.9 init
 terraform_v1.0.9 destroy
-
 popd
+
+aws ec2 describe-instances --query "Reservations[*].Instances[*]" | jq
