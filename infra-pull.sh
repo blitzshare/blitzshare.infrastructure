9f00f7271d3f08ed92490c329781c68bff72c759
Pavel Durov <pav3ldurov@gmail.com>
2021-11-28 08:46:53 +0000

    chore: infra pull

---
+++ infra-pull.sh
@@ -0,0 +1,15 @@
+pushd ../blitzshare.bootstrap.client.cli
+git pull
+popd
+pushd ../blitzshare.event.worker
+git pull
+popd
+pushd ../blitzshare.bootstrap.node
+git pull
+popd
+pushd ../blitzshare.fileshare.worker
+git pull
+popd
+pushd ../blitzshare.infrastructure
+git pull
+popd
