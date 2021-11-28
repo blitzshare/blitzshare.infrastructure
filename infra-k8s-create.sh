08600e35fbb86a1bfd36095898886e3603a16f03
Pavel Durov <pav3ldurov@gmail.com>
2021-11-28 14:15:41 +0000

    chore: k8s infra create

--- infra-k8s-create.sh
+++ infra-k8s-create.sh
@@ -14,15 +14,20 @@ kubectl apply -f https://deploy.kubemq.io/key/0a5e3867-1149-40cf-b9f0-fe8321f524
 # redis store
 kubectl apply -f k8s/redis.yml
 
-
-# k8s bootstrap service
-pushd ../blitzshare.bootstrap.node
-# git pull
+# kubemq worker
+pushd ../blitzshare.event.worker
 make k8s-apply
 popd
 
+# k8s bootstrap service
+pushd ../blitzshare.bootstrap.node
+make k8s-apply
+popd
 
-
+# k8s blitzshare api
+pushd ../blitzshare.api
+make k8s-apply
+popd
 
 # helm repo add kubemq-charts  https://kubemq-io.github.io/charts
 # helm install kubemq-cluster --set key={your-license-key} kubemq-charts/kubemq -n kubemq 
