e0c17a565d06d7f2ad04ede6e99758cc76029bd9
Pavel Durov <pav3ldurov@gmail.com>
2021-11-30 18:38:34 +0000

    chore: k8s infra scripts

--- infra-k8s-create.sh
+++ infra-k8s-create.sh
@@ -1,7 +1,7 @@
 
 
 # setup k8s context
-kubectl ctx docker-desktop
+kubectl ctx minikube
 
 # kube mq
 kubectl apply -f https://deploy.kubemq.io/init
@@ -24,10 +24,3 @@ popd
 pushd ../blitzshare.api
 make k8s-apply
 popd
-
-# helm repo add kubemq-charts  https://kubemq-io.github.io/charts
-# helm install kubemq-cluster --set key={your-license-key} kubemq-charts/kubemq -n kubemq 
-# kubectl ctx blitzshare-cluster
-# bash bin/eks/kluster-load-balancer-deploy.sh
-
-
---
+++ infra-k8s-destroy.sh
@@ -0,0 +1,11 @@
+
+
+# setup k8s context
+kubectl ctx minikube
+
+kubectl delete namespace blitzshare-api-ns
+kubectl delete namespace blitzshare-bootstrap-ns
+kubectl delete namespace blitzshare-redis-ns
+kubectl delete namespace kubemq
+
+
